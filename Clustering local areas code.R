d0d42482670246defec6a444afd80c09ddf960ac
dyl-atk <atkinson.dylan@btinternet.com>
2023-06-18 00:34:01 +0100

    Combined features into one dataframe
    Model features from different datasets integrated into a single dataframe.

--- Clustering local areas code.R
+++ Clustering local areas code.R
@@ -18,6 +18,9 @@ population_density <- read_excel("population_density.xlsx")
 # Ensure attribute names are tidy
 population_density <- population_density %>% clean_names()
 
+# Create distinctive attribute names
+population_density <- population_density %>% rename(population_density = observation)
+
 
 ### Age distribution variable
 
@@ -41,6 +44,21 @@ age_distribution <- age_distribution %>% mutate(total_observations = (aged_15_ye
 
 # Convert age counts to proportions
 age_distribution <- age_distribution %>% mutate_at(vars("aged_15_years_and_under", "aged_16_to_64_years", "aged_65_years_and_over"), ~ . / total_observations) 
+
+
+### Final features dataset
+
+# Bring features to single dataframe
+local_area_data <- full_join(age_distribution, population_density, by = "westminster_parliamentary_constituencies")
+
+# Check there is no missing data following join (desired result is 0 for each attribute)
+local_area_data %>% map(~ sum(is.na(.)))
+
+# Check there is one row per constituency. There should be 573 constituencies in England and Wales
+nrow(local_area_data)
+
+# Select only useful columns for clustering model
+local_area_data <- local_area_data %>% select(westminster_parliamentary_constituencies, aged_15_years_and_under, aged_16_to_64_years, aged_65_years_and_over, population_density)
 
 ##### Data exploration
 
